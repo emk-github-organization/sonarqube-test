f785630e74eea6b27667df30a022de6ebcabd95e
aafetorgbor <85805640+aafetorgbor@users.noreply.github.com>
2024-02-08 12:42:34 -0500

    Create test_square.py

---
+++ test_square.py
@@ -0,0 +1,37 @@
+# Pytest......
+import math
+
+def test_sqrt():
+   num = 25
+   assert math.sqrt(num) == 5
+
+def testsquare():
+   assert 7 * 1 == 7
+
+def tesequality():
+   assert 10 == 10
+
+
+   
+   
+#  # Unittest
+# import unittest
+
+# class TestStringMethods(unittest.TestCase):
+
+#     def test_upper(self):
+#         self.assertEqual('foo'.upper(), 'FOO')
+
+#     def test_isupper(self):
+#         self.assertTrue('FOO'.isupper())
+#         self.assertFalse('Foo'.isupper())
+
+#     def test_split(self):
+#         s = 'hello world'
+#         self.assertEqual(s.split(), ['hello', 'world'])
+#         # check that s.split fails when the separator is not a string
+#         with self.assertRaises(TypeError):
+#             s.split(2)
+
+# if __name__ == '__main__':
+#     unittest.main()
